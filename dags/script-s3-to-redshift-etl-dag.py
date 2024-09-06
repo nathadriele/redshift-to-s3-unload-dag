6ee4b01e25866224d7bc1379c59167a3d68b92e8
Nathalia Adriele <nat-adriele@hotmail.com>
2024-09-06 00:02:42 +0000

    update script

--- dags/script-s3-to-redshift-etl-dag.py
+++ dags/script-s3-to-redshift-etl-dag.py
@@ -5,6 +5,7 @@ from airflow.models.baseoperator import chain
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.models import Variable
+from airflow.exceptions import AirflowException
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,9 +21,15 @@ def get_variable(key, default_value=None):
         logger.warning(f"Variable {key} not found, using default value: {default_value}")
         return default_value
 
+def check_connections(**kwargs):
+    redshift_conn_id = get_variable("REDSHIFT_CONN_ID", "redshift_default")
+    aws_conn_id = get_variable("AWS_CONN_ID", "aws_default")
+    
 S3_BUCKET = get_variable("S3_BUCKET", "default-bucket")
 S3_KEY = get_variable("S3_KEY", "default-key")
 REDSHIFT_TABLE = get_variable("REDSHIFT_TABLE", "default_table")
+REDSHIFT_SCHEMA = get_variable("REDSHIFT_SCHEMA", "public")
+COPY_OPTIONS = get_variable("COPY_OPTIONS", ['csv']).split(',')
 
 with DAG(
     dag_id="s3_to_redshift",
@@ -38,14 +45,20 @@ with DAG(
         dag=dag
     )
 
+    check_connections_task = EmptyOperator(
+        task_id="check_connections",
+        dag=dag,
+        on_failure_callback=check_connections
+    )
+
     # Task to transfer data from S3 to Redshift
     task_transfer_s3_to_redshift = S3ToRedshiftOperator(
         task_id='transfer_s3_to_redshift',
         s3_bucket=S3_BUCKET,
         s3_key=S3_KEY,
-        schema="public",
+        schema=REDSHIFT_SCHEMA,
         table=REDSHIFT_TABLE,
-        copy_options=['csv'],
+        redshift_conn_id=get_variable("REDSHIFT_CONN_ID", "redshift_default"),
     )
 
     logger.info(f"Starting transfer from {S3_BUCKET}/{S3_KEY} to Redshift table {REDSHIFT_TABLE}")
@@ -57,6 +70,7 @@ with DAG(
 
     chain(
         begin,
+        check_connections_task,
         task_transfer_s3_to_redshift,
         end
     )
